843e1e64fcc7b84edc0b57a3a11619ee133a439a
michaelbruce <michael.bruce@singletracksystems.com>
2014-11-06 16:39:26 +0000

    Added Patients AA page, also included a few test components on the Dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -12,22 +12,40 @@ ActiveAdmin.register_page "Dashboard" do
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+     columns do
+       #column do
+       #  panel "Recent Posts" do
+       #    ul do
+       #      Post.recent(5).map do |post|
+       #        li link_to(post.title, admin_post_path(post))
+       #      end
+       #    end
+       #  end
+       #end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+       column do
+         panel "News" do
+           para "Welcome to Medical System."
+         end
+
+         panel "Available Staff" do
+           ul do
+             li "Joseph Smith"
+           end
+         end
+
+         panel "Task History" do
+           para "You have been on call for the past 77 hours."
+         end
+
+         panel "Recent Patients" do
+           table_for Patient.order('updated_at DESC') do
+             column :id
+             column :created_at
+             column :updated_at
+           end
+         end
+       end
+     end
   end # content
 end
